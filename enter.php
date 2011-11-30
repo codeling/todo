5aae92097c8742ead555d6e11d9ef0ea6fa7b541
Bernhard Froehler <bernhard.froehler@gmx.at>
2011-11-30 22:48:17 +0100

    Makes backend enter consider project

--- enter.php
+++ enter.php
@@ -4,6 +4,7 @@
     $todo     = $db->real_escape_string(htmlentities($_REQUEST['todo'], ENT_QUOTES, "UTF-8"));
     $due      = $db->real_escape_string(htmlentities($_REQUEST['due'], ENT_QUOTES, "UTF-8"));
     $priority = (int)$_REQUEST['priority'];
+    $project = $db->real_escape_string(htmlentities($_REQUEST['project'], ENT_QUOTES, "UTF-8"));
     // checks:
     if (!checkDateStr($due)) {
         $due = '';
@@ -14,10 +15,11 @@
     }
     $todo = "'".$todo."'";
     $due  = (strcmp($due, '') == 0) ? "NULL" : "'$due'";
+    $project = "'".$project."'";
     $sql = "INSERT INTO todo ".
             "(creationDate, description, dueDate, priority, notes, project) ".
         "VALUES ".
-            "(UTC_TIMESTAMP(), $todo, $due, $priority, NULL, NULL)";
+            "(UTC_TIMESTAMP(), $todo, $due, $priority, NULL, $project)";
     if ($db->query($sql) == FALSE) {
         echo $db->error;
     } else {
