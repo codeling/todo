0c18839cfb0b8cbd1c262aafd12b0f341084077e
Bernhard Froehler <bernhard.froehler@gmx.at>
2012-02-10 20:51:06 +0100

    Datepicker -> german

--- todo.js
+++ todo.js
@@ -481,23 +481,41 @@ function refresh() {
 //    setTimeout('refresh()', 30000);
 }
 
+function createDatePicker(tagid) {
+    $(tagid).datepicker( {
+        showOn: 'both',
+        buttonImageOnly: true,
+        buttonImage: 'images/calendar.png',
+        dateFormat: 'yy-mm-dd',
+        showAnim: ''
+    });
+}
+
 $(document).ready(function() {
 
-    $("#enter_due").datepicker( {
-        showOn: 'both',
-        buttonImageOnly: true,
-        buttonImage: 'images/calendar.png',
-        dateFormat: 'yy-mm-dd',
-        showAnim: ''
+    jQuery(function($){
+        $.datepicker.regional['de'] = {clearText: 'löschen', clearStatus: 'aktuelles Datum löschen',
+                closeText: 'schließen', closeStatus: 'ohne Änderungen schließen',
+                prevText: '<zurück', prevStatus: 'letzten Monat zeigen',
+                nextText: 'Vor>', nextStatus: 'nächsten Monat zeigen',
+                currentText: 'heute', currentStatus: '',
+                monthNames: ['Januar','Februar','März','April','Mai','Juni',
+                'Juli','August','September','Oktober','November','Dezember'],
+                monthNamesShort: ['Jan','Feb','Mär','Apr','Mai','Jun',
+                'Jul','Aug','Sep','Okt','Nov','Dez'],
+                monthStatus: 'anderen Monat anzeigen', yearStatus: 'anderes Jahr anzeigen',
+                weekHeader: 'Wo', weekStatus: 'Woche des Monats',
+                dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+                dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+                dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+                dayStatus: 'Setze DD als ersten Wochentag', dateStatus: 'Wähle D, M d',
+                dateFormat: 'dd.mm.yy', firstDay: 1,
+                initStatus: 'Wähle ein Datum', isRTL: false};
+        $.datepicker.setDefaults($.datepicker.regional['de']);
     });
-
-    $("#modify_due").datepicker( {
-        showOn: 'both',
-        buttonImageOnly: true,
-        buttonImage: 'images/calendar.png',
-        dateFormat: 'yy-mm-dd',
-        showAnim: ''
-    });
+    
+    createDatePicker("#enter_due");
+    createDatePicker("#modify_due");
 
     $('#modify_save').click(function() {
         // store... 
