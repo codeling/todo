1c1fd274076186154f4656810f7418cacd1cd0dc
Bernhard Froehler <bernhard.froehler@gmx.at>
2013-03-27 20:58:21 +0100

    Makes function static which was called statically before anyway (unearthed by php 5.4)

--- todo-core.php
+++ todo-core.php
@@ -1,15 +1,15 @@
 <?php
 class TodoConstants {
     const AppName = "Simple To Do List";
-	const AppTitle = "To Do";
+    const AppTitle = "To Do";
 }
 
 class TodoLang {
     static $langfile = "lang-de.ini";
     static $langstrings;
-	function _($key) {
-	    return isset(self::$langstrings[$key]) ? self::$langstrings[$key] : '???'.$key.'???';
-	}
+    static function _($key) {
+        return isset(self::$langstrings[$key]) ? self::$langstrings[$key] : '???'.$key.'???';
+    }
 }
 
 TodoLang::$langstrings = parse_ini_file(TodoLang::$langfile);
