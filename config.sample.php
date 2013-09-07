b11562136a54fc724085ebeb49801e1deb92eb89
Bernhard Froehler <bernhard@bfroehler.info>
2013-09-08 01:14:02 +0200

    Updates config.sample.php with new language setting, reformatting

--- config.sample.php
+++ config.sample.php
@@ -1,12 +1,20 @@
 <?php
-// put the hostname of your mysql server here. Usually you can leave "localhost":
+########################################
+# database connection settings:
+
+# the database connection host.
 $db_host     = "localhost";
-
-// the username to use to connect to your mysql database:
-$db_user     = "foo";
-
-// the password to use to connect to your mysql database:
-$db_password = "bar";
-
-// the database to connect to:
+# the database user
+$db_user     = "todo";
+# the database password
+$db_password = "";
+# the database name
 $db_database = "todo";
+
+########################################
+# language settings:
+# the preferred language:
+$language    = "en-US";
+# note: the file with the name $language.ini from lang
+# folder will be used to translate all strings
+
