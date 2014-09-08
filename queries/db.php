ec2e05b5287758c4a66aef96c6c4c41f4b475ba3
Bernhard Froehler <bernhard@bfroehler.info>
2014-09-08 21:41:40 +0200

    Copy tags on reactivate

--- queries/db.php
+++ queries/db.php
@@ -1,28 +1,28 @@
 <?php
 $prefix = (isset($prefix)? $prefix : "../");
 require_once($prefix."config.php");
-	$db = new mysqli($db_host, $db_user, $db_password, $db_database);
+$db = new mysqli($db_host, $db_user, $db_password, $db_database);
 
-	function quoteString($db, $str) {
-		return "'".$db->real_escape_string(htmlentities($str, ENT_QUOTES, "UTF-8"))."'";
-	}
+function quoteString($db, $str) {
+    return "'".$db->real_escape_string(htmlentities($str, ENT_QUOTES, "UTF-8"))."'";
+}
 
-	function dbQueryOrDie($db, $sql) {
-	    $qResult = $db->query($sql);
-		if ($qResult == FALSE) {
-		    echo $db->error;
-			die;
-		}
-		return $qResult;
-	}
+function dbQueryOrDie($db, $sql) {
+    $qResult = $db->query($sql);
+    if ($qResult == FALSE) {
+        echo $db->error;
+        die;
+    }
+    return $qResult;
+}
 
-	function jsonQueryResults($db, $sql)
-	{
-		$qResult = dbQueryOrDie($db, $sql);
-		$allResults = array();
-		while ($stuff = $qResult->fetch_object())
-		{
-			$allResults[] = $stuff;
-		}
-		return json_encode($allResults);
-	}
+function jsonQueryResults($db, $sql)
+{
+    $qResult = dbQueryOrDie($db, $sql);
+    $allResults = array();
+    while ($stuff = $qResult->fetch_object())
+    {
+        $allResults[] = $stuff;
+    }
+    return json_encode($allResults);
+}
--- queries/reactivate-temp.php
+++ queries/reactivate-temp.php
@@ -1,22 +1,36 @@
 <?php
-    $sql = "SELECT UTC_TIMESTAMP()";
-    $qResult = dbQueryOrDie($db, $sql);
-    $creationDate = $qResult->fetch_array()[0];
+$sql = "SELECT UTC_TIMESTAMP()";
+$qResult = dbQueryOrDie($db, $sql);
+$creationDate = $qResult->fetch_array()[0];
+$sql = "SELECT id, description, priority, ".
+        "date_add(completionDate, INTERVAL recurrenceMode DAY) as newDueDate, ".
+        "notes, recurrenceMode, user_id FROM reviving;";
+$qResult = dbQueryOrDie($db, $sql);
+while ($toReactivate = $qResult->fetch_object())
+{
     $sql = "INSERT INTO todo ".
             "(creationDate, description, priority, completed, ".
             "dueDate, notes, version, recurrenceMode, user_id) ".
-        "SELECT '$creationDate' as creationDate, description, priority, 0 as completed, ".
-            "date_add(completionDate, INTERVAL recurrenceMode DAY) as dueDate, ".
-            "notes, 1 as version, recurrenceMode, user_id FROM reviving;";
-    $qResult = dbQueryOrDie($db, $sql) ;
-    $sql = "INSERT INTO recurringCopied (todo_id, copiedDate) SELECT id, '$creationDate' FROM reviving";
-    $qResult = dbQueryOrDie($db, $sql) ;
-    /*
-    TODO: copy tags to new items as well!
-    Problem:
-        - We don't have the IDs of the newly created items
-        - Even if we had those IDs#, We can't easily match the tags of the old items to the new IDs
+            "VALUES (".
+            "'".$creationDate."', ".
+            quoteString($db, $toReactivate->description).", ".
+            $toReactivate->priority.", ".
+            "0, ".   // complated
+            "'".$toReactivate->newDueDate."', ".
+            quoteString($db, $toReactivate->notes).", ".
+            "1, ".   // version
+                $toReactivate->recurrenceMode.", ".
+                $toReactivate->user_id.")";
+    dbQueryOrDie($db, $sql) ;
 
-    $sql = "INSERT INTO todo_tags (todo_id, tag_id) SELECT , tag_id FROM todo_tags t2 WHERE t2.todo_id in (SELECT id FROM reviving)";
-    $qResult = dbQueryOrDie($db, $sql) ;
-    */
+    $newId = $db->insert_id;
+    $sql = "INSERT INTO todo_tags(todo_id, tag_id) ".
+            "SELECT $newId, tag_id FROM todo_tags ".
+            "WHERE todo_id=".$toReactivate->id;
+    dbQueryOrDie($db, $sql) ;
+
+    $sql = "INSERT INTO recurringCopied (todo_id, copiedDate) ".
+            "VALUES (".$toReactivate->id.", ".
+            "'".$creationDate."')";
+    dbQueryOrDie($db, $sql) ;
+}
