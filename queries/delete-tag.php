08dfde146e09a65584148ea892b57e865bc15952
Bernhard Froehler <bernhard@bfroehler.info>
2016-01-02 11:02:37 +0100

    Add ToDo

--- queries/delete-tag.php
+++ queries/delete-tag.php
@@ -1,8 +1,10 @@
 <?php
     require("db.php");
     $id = (int)$_REQUEST['id'];
+	// TODO: restrict to current list!
     dbQueryOrDie($db, "DELETE FROM todo_tags WHERE tag_id=$id");
     $deletedAssignments = $db->affected_rows;
+	// TODO: only delete if no tags left!
     dbQueryOrDie($db, "DELETE FROM tags WHERE id=$id");
     $affectedRows = $db->affected_rows;
     if ($affectedRows < 1) {
--- queries/query-todos.php
+++ queries/query-todos.php
@@ -3,7 +3,7 @@
     require("reactivate.php");
     require("todo-list-query.php");
 
-    // TODO: get ID of logged in user here!
+    // TODO: get ID of logged in user here, verify that list belongs to that user!
     $list_id = (int)$_GET["list_id"];
     $incomplete = isset($_GET["incomplete"])? $_GET["incomplete"] === 'true': false;
     $age = isset($_GET["age"])? (int)$_GET["age"]: 0;
